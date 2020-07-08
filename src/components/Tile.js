20b7a31293e127af7a1f0af24e20bdca4ac1c918
Daniel  Bovington <d.bovington@gmail.com>
2020-07-08 14:39:26 +0100

    imported all images and assigned to variable

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
-function Tile() {
-    return <img />
+function Tile({ src, x, y, z }) {
+    return <img 
+        scr={src}
+        className="tile"
+        style={{ left: `${x}%`, top: `${y}%`, zIndex: z }}
+        alt="tile for landscape"
+    />
 }
 
 export default Tile
